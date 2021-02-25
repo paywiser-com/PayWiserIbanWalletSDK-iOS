e1886e9975f6c16e4897d052fea6628a5777df00
tjasa <tjasaa@gmail.com>
2021-02-25 13:08:08 +0100

    init

---
+++ PayWiserIbanWalletSDK.framework/Headers/PayWiserIbanWalletSDK.h
@@ -0,0 +1,18 @@
+//
+//  PayWiserIbanWalletSDK.h
+//  PayWiserIbanWalletSDK
+//
+//  Created by tjasa on 21/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayWiserIbanWalletSDK.
+FOUNDATION_EXPORT double PayWiserIbanWalletSDKVersionNumber;
+
+//! Project version string for PayWiserIbanWalletSDK.
+FOUNDATION_EXPORT const unsigned char PayWiserIbanWalletSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayWiserIbanWalletSDK/PublicHeader.h>
+
+
